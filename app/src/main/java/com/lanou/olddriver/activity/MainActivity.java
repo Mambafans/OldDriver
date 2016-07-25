fd231bd927f92aff7988d237982b4c94bede18f3
BlackMambaFll <707203994@qq.com>
2016-07-25 15:36:11 +0800

    上传

--- app/src/main/java/com/lanou/olddriver/activity/MainActivity.java
+++ app/src/main/java/com/lanou/olddriver/activity/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SmallPagerFragment smallPagerFrag;
     private MeFragment meFrag;
 
+    private int sts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
